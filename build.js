ce2410c54d7a0dfa74469178810d26c736d52908
Sergey Pogoryelov <pogorelov.s.y@gmail.com>
2018-03-16 17:45:58 +0200

    Added index page and collections support

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const metalsmith = require('metalsmith');
 const markdown = require('metalsmith-markdown');
 const layouts = require('metalsmith-layouts');
+const collections = require('metalsmith-collections');
 
 metalsmith(__dirname)
     .metadata({
@@ -12,6 +13,13 @@ metalsmith(__dirname)
     .clean(true)
     .source('./src')
     .destination('./dist')
+    .use(collections({
+        posts: {
+            pattern: 'posts/**/*.md',
+            sortBy: 'date',
+            reverse: true
+        }
+    }))
     .use(markdown())
     .use(layouts({
         directory: './layouts',
--- src/posts/test.md
+++ src/posts/test.md
@@ -1,5 +1,6 @@
 ---
 title: 'testTitle'
+date: 2018-03-15
 ---
 # Test
 
