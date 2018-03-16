b43eb9cb52b1f565cb6ac48e3c392fd3e7cbc039
Sergey Pogoryelov <pogorelov.s.y@gmail.com>
2018-03-16 14:13:48 +0200

    Established transformation of markdown to HTML

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 const metalsmith = require('metalsmith');
+const markdown = require('metalsmith-markdown');
 
 metalsmith(__dirname)
     .metadata({
@@ -10,6 +11,7 @@ metalsmith(__dirname)
     .clean(true)
     .source('./src')
     .destination('./dist')
+    .use(markdown())
     .build((err) => {
         if (err) {
             console.error(err);
---
+++ src/test.md
@@ -0,0 +1,3 @@
+# Test
+
+__it works!__
