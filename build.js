70445a02f52a92a2e1052053fbdf021f08fd5a35
Sergey Pogoryelov <pogorelov.s.y@gmail.com>
2018-03-16 17:49:38 +0200

    Added permalinks support (fixed navigation from index)

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const metalsmith = require('metalsmith');
 const markdown = require('metalsmith-markdown');
 const layouts = require('metalsmith-layouts');
 const collections = require('metalsmith-collections');
+const permalinks = require('metalsmith-permalinks');
 
 metalsmith(__dirname)
     .metadata({
@@ -21,9 +22,13 @@ metalsmith(__dirname)
         }
     }))
     .use(markdown())
+    .use(permalinks({
+        relative: false,
+        pattern: ':title'
+    }))
     .use(layouts({
         directory: './layouts',
-        default: 'article.hbs'
+        default: 'post.hbs'
     }))
     .build((err) => {
         if (err) {
