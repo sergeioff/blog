3615081d84c70bf8be69aee33d96d93a9b820fdf
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2020-02-04 05:10:42 +0200

    Initial gatsby configuration

---
+++ gatsby-config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  siteMetadata: {
+    title: `Sergey Pogorelov's blog`,
+    name: `Pogorelovs`,
+    siteUrl: `https://novela.narative.co`,
+    description: `Sergey Pogorelov's blog`,
+    hero: {
+      heading: `Welcome to my blog,
+      here I write my thoughts and notes about the development.`,
+      maxWidth: 640,
+    },
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/sergeioff`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/sergeioff`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        authorsPage: true,
+        sources: {
+          local: true,
+          // contentful: true,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Sergey Pogorelov's blog`,
+        short_name: `Pogorelovs`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
+      },
+    },
+  ],
+};
