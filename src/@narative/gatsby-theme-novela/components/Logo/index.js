b9cc99af182c9797b8fb84e391f06919ac7b28a1
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2020-02-04 07:47:53 +0200

    fix(logo): default plain text logo

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -7,10 +7,6 @@ import React from 'react';
  */
 export default function Logo() {
   return (
-    <svg viewBox="0 0 106 28" height="30px">
-      <path d="M62.9 12h2.8v10...." />
-      <path fill="#fff" d="M25 14h-..." />
-      <path d="M14 0C6.3..." fill="#639" />
-    </svg>
+    <h2>Pogorelovs</h2>
   );
 }
